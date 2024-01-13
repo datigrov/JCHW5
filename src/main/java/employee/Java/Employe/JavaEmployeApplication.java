a32c8452b528cba74ad5e29fceec66b9dde47e08
datigrov <denistigrov@icloud.com>
2024-01-13 17:37:00 +0300

    add gitignore

---
+++ src/main/java/employee/Java/Employe/JavaEmployeApplication.java
@@ -0,0 +1,13 @@
+package employee.Java.Employe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmployeApplication.class, args);
+	}
+
+}
